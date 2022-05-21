1183400f5a8c3a43f1ad65fa59ea12ace483057c
devprajapt02 <devpraapat02@gmail.com>
2022-05-21 22:40:52 +0530

    Initial Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
 import './App.css';
+import Web3 from 'web3';
 
 class App extends Component {
+
+  async componentWillMount(){
+    await this.loadWeb3();
+    await this.loadBlockchain();
+  }
+
+  async loadBlockchain(){
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts);
+  }
+
+  async loadWeb3(){
+    if(window.etherium){
+      window.web3 = new Web3(window.etherium);
+      await window.etherium.enable();
+      console.log('aaaa');
+    }else if(window.web3){
+      window.web3 = new Web3(window.web3.currentProvider);
+      console.log('bbbb');
+    }else{
+      window.alert("Etherium browser not deteted");
+    }
+  }
+
   render() {
     return (
       <div>
@@ -13,32 +38,16 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapp University
+            Home
           </a>
         </nav>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+                
+                <h1>Dapp University</h1>
+               
               </div>
             </main>
           </div>
