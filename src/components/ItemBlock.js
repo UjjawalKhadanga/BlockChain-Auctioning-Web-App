e245f6708866be80098d961729214219a7b85368
devprajapt02 <devpraapat02@gmail.com>
2022-05-22 20:59:15 +0530

    Solidity

---
+++ src/components/ItemBlock.js
@@ -0,0 +1,17 @@
+import React from 'react'
+export default function ItemBlock() {
+  return (
+    <>
+        <div>
+        <div className="card" style={{width: '18rem'}}>
+        <img src="https://media-exp1.licdn.com/dms/image/C4E0BAQGr4jMwOp7BBA/company-logo_200_200/0/1519901877144?e=2147483647&v=beta&t=OofZYhgvbE15nPCWqj_deuhcHZQq5BQcN755zIDjS24" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+        </div>
+    </>
+  )
+}
---
+++ src/components/ItemList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ItemBlock from './ItemBlock'
+
+export default function ItemList() {
+  return (
+    <div>
+        <ItemBlock />
+    </div>
+  )
+}
